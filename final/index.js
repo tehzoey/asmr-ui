85dcd08e6652bd0f01c64dd91da899c574b184a8
tehzoey <61260075+tehzoey@users.noreply.github.com>
2020-02-20 10:48:59 +0800

    Add files via upload

---
+++ final/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // Import stylesheets
+    import './style.css';
+
+    $('#button').click(function (e) { 
+        window.location.href = "loading.html";
+        
+    });
+});
+
+
